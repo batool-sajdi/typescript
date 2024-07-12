2b8990c12f08307a9d4b10e966aa0e2a08f1b4ec
batool_sajdi <btool.edu@gmail.com>
2024-07-12 04:35:14 -0700

    next code

---
+++ day2/main.js
@@ -0,0 +1,10 @@
+var myname = "sajdi"; //     infarence type
+var mynumber = 1234;
+myname = "infaer";
+mynumber = 4535437;
+var mynumber1 = 12345;
+var myname1 = "strong"; //   strong typing
+console.log(myname);
+console.log(myname1);
+console.log(mynumber);
+console.log(mynumber1);
---
+++ day2/main.ts
@@ -0,0 +1,16 @@
+let myname="sajdi"; //     infarence type
+
+let mynumber=1234;
+
+myname="infaer";
+
+mynumber=4535437;
+let mynumber1:number=12345;
+
+let myname1:string="strong"; //   strong typing
+
+console.log(myname)
+console.log(myname1)
+
+console.log(mynumber)
+console.log(mynumber1)
