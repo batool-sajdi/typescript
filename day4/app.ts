0e0b728ecb55b8095653db5e796d1a5121bfa42e
batool_sajdi <btool.edu@gmail.com>
2024-07-14 05:06:53 -0700

    add mudules and expnantion

--- day4/app.ts
+++ day4/app.ts
@@ -13,6 +13,24 @@ console.log(`addetion 12+3= ${addetion}`);
 
 console.log(`subterction 12-3= ${subterction}`);
 
+
+
+
 console.log(`devsion 12/3= ${devsion}`);
 
 console.log(`multiplycation 12*3 =${multiplycation}`)
+
+
+
+let five=5;
+let two =2;
+
+let xpo=five**two;
+
+console.log(`xpo ${xpo}`)
+
+
+let mod=75;
+let mod1=10;
+let res=mod%mod1
+console.log(res)
